18015433920cdf6e985c61b288cc58630ad92936
Sadikhali p v <105201040+Sadikhal@users.noreply.github.com>
2025-11-05 11:06:56 +0530

    responsive

--- app/page.tsx
+++ app/page.tsx
@@ -79,8 +79,8 @@ const Home = () => {
             className="
             pt-34
             grid
-            grid-cols-2
-            sm:grid-cols-2
+            grid-cols-1
+            md:grid-cols-2
             lg:grid-cols-3
             xl:grid-cols-4
             2xl:grid-cols-5
